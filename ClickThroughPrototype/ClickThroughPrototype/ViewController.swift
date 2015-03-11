52cb39a3f833163c673ea8ed204ad6cf427fa5f1
Stacey Adams <sleung@zynga.com>
2015-03-11 14:13:26 -0700

    click through prototype

---
+++ ClickThroughPrototype/ClickThroughPrototype/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ClickThroughPrototype
+//
+//  Created by Stacey Adams on 3/11/15.
+//  Copyright (c) 2015 Stacey & Bryan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
