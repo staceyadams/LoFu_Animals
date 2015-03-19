6744743df499401d28fec3423ee9f50fbd5de9ed
Bryan Leung <brleung@zynga.com>
2015-03-18 21:08:45 -0700

    Made writing view controller work
    =)

--- App/LoFuAnimals/LoFuAnimals/WritingViewController.swift
+++ App/LoFuAnimals/LoFuAnimals/WritingViewController.swift
@@ -8,27 +8,33 @@
 
 import UIKit
 
-class WritingViewController: UIViewController
+class WritingViewController: UIViewController, UITextFieldDelegate
 {
 
     @IBOutlet weak var writingTextField: UITextField!
     @IBOutlet weak var writingImage: UIImageView!
+    @IBOutlet weak var displayWord: UILabel!
+    @IBOutlet weak var backgroundTile: UIView!
     
-    var animalsText: [String] = ["cat", "dog", "fish", "rabbit", "bird", "hamster"]
-    var animalsCards: [String] = ["flashcard_cat", "flashcard_dog", "flashcard_fish", "flashcard_rabbit", "flashcard_bird", "flashcard_hamster"]
+    var animalsText: [String] = ["猫", "狗", "fish", "rabbit", "bird", "hamster"]
+    var animalsCards: [String] = ["cat", "dog", "fish", "rabbit", "bird", "hamster"]
     var currentWord: String!
     var currentWordImage: String!
     
-    var currentPosition: Int = -1 // We increment before using, so set to -1 to begin
+    var currentPosition: Int = 0 // We increment before using, so set to -1 to begin
     
     
     override func viewDidLoad()
     {
         super.viewDidLoad()
+        
+        backgroundTile.backgroundColor = UIColor(patternImage: UIImage(named: "bg-pets")!)
+        writingTextField.delegate = self
       
         currentWord = animalsText[0]
         currentWordImage = animalsCards[0]
         writingImage.image = UIImage(named:currentWordImage)
+        displayWord.text = animalsText[0]
     }
 
     override func didReceiveMemoryWarning() {
@@ -42,32 +48,20 @@ class WritingViewController: UIViewController
         {
            // setNextAnimal()
             
-            println("\(currentWord) + \(currentWordImage)")
-            println(currentPosition)
+//println("\(currentWord) + \(currentWordImage)")
+            // println(currentPosition)
             
             getNextAnimal()
             
-            // currentPosition points to the *back* card, we want to remove the *front*.
-            // Make sure if back was at the start of array, we consider front to be end.
-            var indexToRemove = currentPosition - 1
-            if (indexToRemove == -1) { indexToRemove = animalsText.count - 1 }
-            
-            // Remove the element
-            animalsText.removeAtIndex(indexToRemove)
-            
-            // If position wasn't the start, we need to decrement. If it *was* the start
-            // then we removed from the end, so there's no change to make to currentPosition.
-            if (currentPosition > 0 ) { currentPosition-- }
-            
-            // If that position was the final item, currentPosition will no longer be valid.
-            if (currentPosition == animalsText.count) {currentPosition = 0}
-            
+            displayWord.text = animalsText[currentPosition]
             currentWord = animalsText[currentPosition]
             currentWordImage = animalsCards[currentPosition]
             writingImage.image = UIImage(named:currentWordImage)
             
-            println(currentPosition)
-            println("\(currentWord) + \(currentWordImage)")
+            //println(currentPosition)
+            //println("\(currentWord) + \(currentWordImage)")
+            
+            writingTextField.resignFirstResponder()
             
         }
         else
@@ -82,32 +76,27 @@ class WritingViewController: UIViewController
         }
     }
     
-    func setNextAnimal()
-    {
-        // hacky way to move through the index by removing each one as its completed, and resetting current to the first item
-        animalsText.removeAtIndex(0)
-        animalsCards.removeAtIndex(0)
-        currentWord = animalsText[0]
-        currentWordImage = animalsCards[0]
-        writingImage.image = UIImage(named:currentWordImage)
-    }
-    
     func getNextAnimal() -> String
     {
         currentPosition++ // Move to next position
-        if (currentPosition == animalsText.count) { currentPosition = 0; } // Make sure we loop around
+        if (currentPosition == animalsText.count) {
+            
+            currentPosition = animalsText.count; // Keep the image the sam as the final one
+            
+            //Do the Segue
+            //prepareForSegue("waiting", sender: nil)
+            println("SEGUE NOW!")
+        }
+        
         return animalsText[currentPosition]
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        
+        textField.resignFirstResponder()
+        
+        return true;
+        
     }
-    */
 
 }
