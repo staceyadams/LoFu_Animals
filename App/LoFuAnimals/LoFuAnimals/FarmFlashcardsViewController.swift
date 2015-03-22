912e10dbd9ee020607c6b61c0e06c346e28ec083
Stacey Adams <sleung@zynga.com>
2015-03-22 12:07:38 -0700

    add farm animals

--- App/LoFuAnimals/LoFuAnimals/FarmFlashcardsViewController.swift
+++ App/LoFuAnimals/LoFuAnimals/FarmFlashcardsViewController.swift
@@ -1,31 +1,28 @@
 //
-//  FlashcardViewController.swift
+//  FarmFlashcardsViewController.swift
 //  LoFuAnimals
 //
-//  Created by Stacey Adams on 3/14/15.
+//  Created by Stacey Adams on 3/22/15.
 //  Copyright (c) 2015 Stacey & Bryan. All rights reserved.
 //
-
-// reference ChoosePersonViewController.m in the LikedorNope example for all the things we are customizing
-
 import UIKit
 import MDCSwipeToChoose
 
-class FlashcardViewController: UIViewController, MDCSwipeToChooseDelegate {
+class FarmFlashcardViewController: UIViewController, MDCSwipeToChooseDelegate {
     
-    var animals: [String] = ["flashcard_cat", "flashcard_dog", "flashcard_fish", "flashcard_rabbit", "flashcard_bird", "flashcard_hamster"]
+    @IBOutlet weak var cardsLeftLabel: UILabel!
+    
+    var animals: [String] = ["flashcard_sheep", "flashcard_cow", "flashcard_pig", "flashcard_rooster", "flashcard_hen", "flashcard_horse"]
     
     var currentPosition: Int = -1 // We increment before using, so set to -1 to begin
     var frontCardView: MDCSwipeToChooseView!
     var backCardView: MDCSwipeToChooseView!
-
-    @IBOutlet weak var cardsLeftLabel: UILabel!
-    
+        
     override func viewDidLoad()
     {
         super.viewDidLoad()
         
-        view.backgroundColor = UIColor(patternImage: UIImage(named: "bg-pets")!)
+        view.backgroundColor = UIColor(patternImage: UIImage(named: "bg-farm")!)
         
         frontCardView = popCardViewWithFrame(frontCardViewFrame(), animal: getNextAnimal())
         frontCardView.layer.borderColor = UIColor.clearColor().CGColor
@@ -38,7 +35,7 @@ class FlashcardViewController: UIViewController, MDCSwipeToChooseDelegate {
         cardsLeftLabel.text = "\(animals.count)"
         cardsLeftLabelAnimate()
     }
-
+    
     
     // Returns the next animal from array, (loops back to beginning if we're at the end), and increment
     // the pointer to the next position. We can call this multiple times to keep moving to the next, etc.
@@ -60,10 +57,10 @@ class FlashcardViewController: UIViewController, MDCSwipeToChooseDelegate {
         options.delegate = self
         options.threshold = 10
         options.onPan =
-        {
-            (state: MDCPanState!) in
-            let frame = self.backCardViewFrame()
-            self.backCardView.frame = CGRectMake(frame.origin.x - state.thresholdRatio * CGFloat(5), frame.origin.y - state.thresholdRatio * CGFloat(5), CGRectGetWidth(frame), CGRectGetHeight(frame))
+            {
+                (state: MDCPanState!) in
+                let frame = self.backCardViewFrame()
+                self.backCardView.frame = CGRectMake(frame.origin.x - state.thresholdRatio * CGFloat(5), frame.origin.y - state.thresholdRatio * CGFloat(5), CGRectGetWidth(frame), CGRectGetHeight(frame))
         }
         // hide the text because I can't figure out how to customize on init
         options.likedText = "✅ Learned!"
@@ -85,9 +82,8 @@ class FlashcardViewController: UIViewController, MDCSwipeToChooseDelegate {
         let horizontalPadding: CGFloat = 15
         let topPadding: CGFloat = 70
         let bottomPadding: CGFloat = 0
-//        return CGRectMake(horizontalPadding, topPadding, CGRectGetWidth(self.view.frame) - (horizontalPadding*2), CGRectGetHeight(self.view.frame) - (bottomPadding))
         var cardRect = CGRectMake(horizontalPadding, topPadding, 290, 372)
-
+        
         
         return cardRect // set size based on image size instead of screen space
     }
@@ -146,7 +142,7 @@ class FlashcardViewController: UIViewController, MDCSwipeToChooseDelegate {
         // If all cards are gone, segue to summary screen
         if (animals.count == 0)
         {
-            performSegueWithIdentifier("summarySegue", sender: self)
+//            performSegueWithIdentifier("summarySegue", sender: self)
             return
         }
         
@@ -161,30 +157,11 @@ class FlashcardViewController: UIViewController, MDCSwipeToChooseDelegate {
             backCardView.alpha = 0
             self.view.insertSubview(backCardView, belowSubview: frontCardView)
             UIView.animateWithDuration(0.2, animations:
-            { () -> Void in
-                self.backCardView.alpha = 1
-            }, completion: nil)
+                { () -> Void in
+                    self.backCardView.alpha = 1
+                }, completion: nil)
         }
     }
-    
-    // @todo why does it error on the buttons but not on the swipes??
-//    @IBAction func pressLater(sender: AnyObject)
-//    {
-//        frontCardView.mdc_swipe(MDCSwipeDirection.Left)
-//    }
-//
-//    @IBAction func pressLearned(sender: AnyObject)
-//    {
-//        frontCardView.mdc_swipe(MDCSwipeDirection.Right)
-//    }
-//    
-//
-//    // Back button
-//    @IBAction func didClickBackButton(sender: AnyObject)
-//    {
-//        navigationController!.popViewControllerAnimated(true)
-//    }
-    
     
     func cardsLeftLabelAnimate()
     {
@@ -200,9 +177,9 @@ class FlashcardViewController: UIViewController, MDCSwipeToChooseDelegate {
                         self.cardsLeftLabel.transform = CGAffineTransformMakeScale(1, 1)
                     })
                     { (Bool) -> Void in }
-            }
+        }
     }
-
+    
     @IBAction func didPressHome(sender: AnyObject)
     {navigationController!.popViewControllerAnimated(true)}
     
