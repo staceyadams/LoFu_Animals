4ba692d6fcac148e2f6484efbe44bee054ed6159
Stacey Adams <sleung@zynga.com>
2015-03-22 19:51:46 -0700

    intro animation progress

--- App/LoFuAnimals/LoFuAnimals/WelcomeViewController.swift
+++ App/LoFuAnimals/LoFuAnimals/WelcomeViewController.swift
@@ -18,6 +18,19 @@ class WelcomeViewController: UIViewController, UIScrollViewDelegate
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var startButton: UIButton!
     
+    var cardViewStartX: CGFloat! = 204
+    var cardViewStartY: [CGFloat] = [0, 10, 20, 30, 40, 50]
+    var cardViewEndX: [CGFloat] = [2, 106, 210, 2, 106, 210]
+    var cardViewEndY: [CGFloat] = [0, 0, 0, 135, 135, 135]
+    var animalImageStartX: CGFloat! = 21
+    var animalImageStartY: [CGFloat] = [16, 36, 56, 76, 96, 116]
+    var animalImageEndX: [CGFloat] = [30, 135, 240, 30, 135, 240]
+    var animalImageEndY: [CGFloat] = [65, 65, 65, 200, 200, 200]
+
+    var cardText: [String] = ["猫", "狗", "鱼", "兔子", "鸟", "仓鼠"]
+    var animalSticker: [String] = ["sticker_cat", "sticker_dog", "sticker_fish", "sticker_rabbit", "sticker_bird", "sticker_hamster"]
+
+    
     override func viewDidLoad()
     {
         super.viewDidLoad()
@@ -25,59 +38,13 @@ class WelcomeViewController: UIViewController, UIScrollViewDelegate
         // Do any additional setup after loading the view.
         scrollView.delegate = self
         scrollView.contentSize = CGSize(width: 320, height: 1200)
+        cardsStartPosition()
         
         for index in 0...5
         {
-//            println("animalImage: \(animalImage[index].frame.origin.x), \(animalImage[index].frame.origin.y)")
-//            println("cardView: \(cardView[index].frame.origin.x), \(cardView[index].frame.origin.y)")
-//            println("cardLabel: \(cardLabel[index].frame.origin.x), \(cardLabel[index].frame.origin.y)")
-            
+            cardLabel[index].text = cardText[index]
+            animalImage[index].image = UIImage(named: "animalSticker[index]")
         }
-        
-        /*
-        STARTING X/Y
-        animalImage: 21.0, 16.0
-        cardView: 204.0, 0.0
-        cardLabel: 17.0, 14.0
-        animalImage: 21.0, 36.0
-        cardView: 204.0, 10.0
-        cardLabel: 17.0, 14.0
-        animalImage: 21.0, 56.0
-        cardView: 204.0, 20.0
-        cardLabel: 17.0, 14.0
-        animalImage: 21.0, 76.0
-        cardView: 204.0, 30.0
-        cardLabel: 17.0, 14.0
-        animalImage: 21.0, 96.0
-        cardView: 204.0, 40.0
-        cardLabel: 17.0, 14.0
-        animalImage: 21.0, 116.0
-        cardView: 204.0, 50.0
-        cardLabel: 17.0, 14.0
-        
-        
-        FINAL X/Y
-        animalImage: 30.0, 65.0
-        cardView: 2.0, 0.0
-        cardLabel: 17.0, 14.0
-        animalImage: 135.0, 65.0
-        cardView: 106.0, 0.0
-        cardLabel: 17.0, 14.0
-        animalImage: 240.0, 65.0
-        cardView: 210.0, 0.0
-        cardLabel: 17.0, 14.0
-        animalImage: 30.0, 200.0
-        cardView: 2.0, 135.0
-        cardLabel: 17.0, 14.0
-        animalImage: 135.0, 200.0
-        cardView: 106.0, 135.0
-        cardLabel: 17.0, 14.0
-        animalImage: 235.0, 200.0
-        cardView: 210.0, 135.0
-        cardLabel: 17.0, 14.0
-        */
-        
-        
         
     }
 
@@ -89,12 +56,40 @@ class WelcomeViewController: UIViewController, UIScrollViewDelegate
     
     func scrollViewDidScroll(scrollView: UIScrollView)
     {
-    
+        var offset = Float(scrollView.contentOffset.y)
+        if offset > 0 {cardsEndPosition()}
+        else {cardsStartPosition()}
+        
     }
-
-    @IBAction func onPressStart(sender: AnyObject)
+    
+    func cardsStartPosition()
     {
-        println("i'm being clicked")
+        for index in 0...5
+        {
+            UIView.animateWithDuration(0.5, animations:
+            { () -> Void in
+                self.cardView[index].frame.origin.x = self.cardViewStartX
+                self.cardView[index].frame.origin.y = self.cardViewStartY[index]
+                self.animalImage[index].frame.origin.x = self.animalImageStartX
+                self.animalImage[index].frame.origin.y = self.animalImageStartY[index]
+            })
+        }
+        
+    }
+    
+    func cardsEndPosition()
+    {
+        for index in 0...5
+        {
+            UIView.animateWithDuration(0.5, animations:
+            { () -> Void in
+                self.cardView[index].frame.origin.x = self.cardViewEndX[index]
+                self.cardView[index].frame.origin.y = self.cardViewEndY[index]
+                self.animalImage[index].frame.origin.x = self.animalImageEndX[index]
+                self.animalImage[index].frame.origin.y = self.animalImageEndY[index]
+            })
+        }
+        
     }
     
 }
