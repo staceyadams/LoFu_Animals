6ca79e30671e7da9e9d4588353118576b07964f1
Stacey Adams <sleung@zynga.com>
2015-03-22 17:19:39 -0700

    set up intro screen

--- App/LoFuAnimals/LoFuAnimals/WelcomeViewController.swift
+++ App/LoFuAnimals/LoFuAnimals/WelcomeViewController.swift
@@ -11,8 +11,11 @@ import UIKit
 class WelcomeViewController: UIViewController, UIScrollViewDelegate
 {
 
+    @IBOutlet var cardView: [UIView]!
+    @IBOutlet var cardLabel: [UILabel]!
+    @IBOutlet var animalImage: [UIImageView]!
+    
     @IBOutlet weak var scrollView: UIScrollView!
-    @IBOutlet weak var pageControl: UIPageControl!
     @IBOutlet weak var startButton: UIButton!
     
     override func viewDidLoad()
@@ -21,7 +24,61 @@ class WelcomeViewController: UIViewController, UIScrollViewDelegate
             
         // Do any additional setup after loading the view.
         scrollView.delegate = self
-        scrollView.contentSize = CGSize(width: 1280, height: 568)
+        scrollView.contentSize = CGSize(width: 320, height: 1200)
+        
+        for index in 0...5
+        {
+//            println("animalImage: \(animalImage[index].frame.origin.x), \(animalImage[index].frame.origin.y)")
+//            println("cardView: \(cardView[index].frame.origin.x), \(cardView[index].frame.origin.y)")
+//            println("cardLabel: \(cardLabel[index].frame.origin.x), \(cardLabel[index].frame.origin.y)")
+            
+        }
+        
+        /*
+        STARTING X/Y
+        animalImage: 21.0, 16.0
+        cardView: 204.0, 0.0
+        cardLabel: 17.0, 14.0
+        animalImage: 21.0, 36.0
+        cardView: 204.0, 10.0
+        cardLabel: 17.0, 14.0
+        animalImage: 21.0, 56.0
+        cardView: 204.0, 20.0
+        cardLabel: 17.0, 14.0
+        animalImage: 21.0, 76.0
+        cardView: 204.0, 30.0
+        cardLabel: 17.0, 14.0
+        animalImage: 21.0, 96.0
+        cardView: 204.0, 40.0
+        cardLabel: 17.0, 14.0
+        animalImage: 21.0, 116.0
+        cardView: 204.0, 50.0
+        cardLabel: 17.0, 14.0
+        
+        
+        FINAL X/Y
+        animalImage: 30.0, 65.0
+        cardView: 2.0, 0.0
+        cardLabel: 17.0, 14.0
+        animalImage: 135.0, 65.0
+        cardView: 106.0, 0.0
+        cardLabel: 17.0, 14.0
+        animalImage: 240.0, 65.0
+        cardView: 210.0, 0.0
+        cardLabel: 17.0, 14.0
+        animalImage: 30.0, 200.0
+        cardView: 2.0, 135.0
+        cardLabel: 17.0, 14.0
+        animalImage: 135.0, 200.0
+        cardView: 106.0, 135.0
+        cardLabel: 17.0, 14.0
+        animalImage: 235.0, 200.0
+        cardView: 210.0, 135.0
+        cardLabel: 17.0, 14.0
+        */
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning()
@@ -32,23 +89,7 @@ class WelcomeViewController: UIViewController, UIScrollViewDelegate
     
     func scrollViewDidScroll(scrollView: UIScrollView)
     {
-        var page = Int(scrollView.contentOffset.x / 320)
-        pageControl.currentPage = page
-        // on page 4, hide the page control and show the button
-        if (page == 3)
-        {
-            UIView.animateWithDuration(0.2, animations:
-            {
-                () -> Void in
-                self.pageControl.alpha = 0
-                self.startButton.alpha = 1
-            })
-        }
-        else
-        {
-            pageControl.alpha = 1
-            startButton.alpha = 0
-        }
+    
     }
 
     @IBAction func onPressStart(sender: AnyObject)
